4e015b96221e67acc17962640da46d827484588e
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-09-04 06:18:50 +0900

    First Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to shade
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/shade/core_test.clj
@@ -0,0 +1,7 @@
+(ns shade.core-test
+  (:require [clojure.test :refer :all]
+            [shade.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
