01452196f2783522c64a724b75d7ab3a102c3fae
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-09-07 09:09:10 +0900

    almost shade but little bit different
    see todo

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/Miyoshi-Ryota/SHADE-clojure"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [mrcsce/normal-distribution "0.1.0"]]
   :repl-options {:init-ns shade.core})
--- src/shade/core.clj
+++ src/shade/core.clj
@@ -1,11 +1,12 @@
 (ns shade.core
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s])
+  (:require [normal-distribution.core :as n]
+            [clojure.set :as set]))
 
 "ToDo:
-* muationの足し算などをベクトル演算できるようにする
-* 子をfitnessがいい場合のみ置き換えるようにする
-* SHADEのパラメータ調整を実装する
-* もしかしたら、solutionをmapにしてgenとfitnessを持つようにした方がいいかも"
+* mfのアップデート方法とmcrのアップデート方法は本当は違うが、今回は同じにしてしまっているので治すこと
+* fのノイズもコーシー分布からとるようにすること
+"
 
 (defn foo
   "I don't do a whole lot."
@@ -46,7 +47,10 @@
 
 (s/def ::data (s/coll-of number?))
 (s/def ::fitness (s/nilable number?))
-(s/def ::solution (s/keys :req [::data ::fitness]))
+(s/def ::crossover-rate (s/nilable number?))
+(s/def ::scaling-factor (s/nilable number?))
+(s/def ::improved-fitness (s/nilable number?))
+(s/def ::solution (s/keys :req [::data ::fitness ::crossover-rate ::scaling-factor ::improved-fitness]))
 
 (s/def ::population (s/coll-of ::solution))
 
@@ -61,7 +65,10 @@
   (let [solution-data (->> (partial generate-rand-between -5 5)
                            (repeatedly dimension))]
     (hash-map ::data solution-data
-              ::fitness (evaluate-function solution-data))))
+              ::fitness (evaluate-function solution-data)
+              ::crossover-rate nil
+              ::scaling-factor nil
+              ::improved-fitness nil)))
 
 (defn- initialize-population
   [population-size dimension lower-bound upper-bound evaluate-function]
@@ -90,9 +97,13 @@
   {:pre [(s/valid? ::solution parent-sol)
          (s/valid? ::solution child-sol)]
    :post [(s/valid? ::solution %)]}
-  (let [crossed-over-data (map #(crossover-one-gene %1 %2 crossover-rate) (::data parent-sol) (::data child-sol))]
+  (let [crossed-over-data (map #(crossover-one-gene %1 %2 crossover-rate) (::data parent-sol) (::data child-sol))
+        fitness (evaluate-function crossed-over-data)]
     (hash-map ::data crossed-over-data
-              ::fitness (evaluate-function crossed-over-data))))
+              ::fitness fitness
+              ::crossover-rate crossover-rate
+              ::scaling-factor (::scaling-factor child-sol)
+              ::improved-fitness (- (::fitness parent-sol) fitness))))
 
 (defn- get-pbest-solution
   [p population]
@@ -131,14 +142,17 @@
         x_pbest (::data (get-pbest-solution 0.2 population))
         mutated-data (map #(mutation-one-gene %1 %2 %3 %4 scaling-factor) x x_r1 x_r2 x_pbest)]
     (hash-map ::data mutated-data
-              ::fitness nil)))
+              ::fitness nil
+              ::scaling-factor scaling-factor
+              ::crossover-rate nil
+              ::improved-fitness nil)))
 
 (defn- select-better-solution
   [solution1 solution2]
   {:pre [(s/valid? ::solution solution1)
          (s/valid? ::solution solution2)]
    :post [(s/valid? ::solution %)]}
-  (if (< (::fitness solution1) (::fitness solution2)) solution1
+  (if (<= (::fitness solution1) (::fitness solution2)) solution1
                                                       solution2))
 
 (defn- select-next-population
@@ -149,15 +163,60 @@
    :post [(s/valid? ::population %)]}
   (map select-better-solution now-pop crossed-over-pop))
 
+(defn- add-gaussian-noise
+  [n]
+  {:pre [(s/valid? number? n)]
+   :post [(s/valid? number? %)]}
+  (n/sample-normal-distribution n 0.1))
+
+(defn- update-memory-crossover-rate
+  [memory-crossover-rate
+   memory-index
+   successful-crossover-rate
+   improved-fitness]
+  (if (= 0 (count successful-crossover-rate))
+    memory-crossover-rate
+    (let [sum-of-improved (apply + improved-fitness)
+          weight (map #(/ % sum-of-improved) improved-fitness)]
+      (->> (map #(* %1 %2) weight successful-crossover-rate)
+           (apply +)
+           (assoc memory-crossover-rate memory-index)))))
+
+(defn- update-memory-scaling-factor
+  [memory-scaling-factor
+   memory-index
+   successful-scaling-factor
+   improved-fitness]
+  (if (= 0 (count successful-scaling-factor))
+    memory-scaling-factor
+    (let [sum-of-improved (apply + improved-fitness)
+          weight (map #(/ % sum-of-improved) improved-fitness)]
+      (->> (map #(* %1 %2) weight successful-scaling-factor)
+           (apply +)
+           (assoc memory-scaling-factor memory-index)))))
+
 (defn run
   [population-size dimension min_x max_x evaluate-function max_generation]
   (loop [population (initialize-population population-size dimension min_x max_x evaluate-function)
-         generation 1]
+         generation 1
+         memory-crossover-rate [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5]
+         memory-scaling-factor [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5]]
     (if (<= max_generation generation)
       population
-      (let [children (->> population
-                          (map #(mutation % 0.5 population))
-                          (map #(crossover %1 %2 0.5 evaluate-function) population))]
-        (recur (select-next-population population children) (inc generation))))))
+      (let [memory-index (mod generation 8)
+            crossover-rates (repeatedly (partial add-gaussian-noise (nth memory-crossover-rate memory-index)))
+            scaling-factors (repeatedly (partial add-gaussian-noise (nth memory-scaling-factor memory-index)))
+            children (->> population
+                          (map #(mutation %2 %1 population) scaling-factors)
+                          (map #(crossover %1 %3 %2 evaluate-function) population crossover-rates))
+            next-generation-population (select-next-population population children)
+            successful-children (set/difference (set next-generation-population) (set population))
+            successful-crossover-rate (map ::crossover-rate successful-children)
+            successful-scaling-factor (map ::scaling-factor successful-children)
+            improved-fitness (map ::improved-fitness successful-children)
+            memory-crossover-rate (update-memory-crossover-rate memory-crossover-rate memory-index successful-crossover-rate improved-fitness)
+            memory-scaling-factor (update-memory-scaling-factor memory-scaling-factor memory-index successful-scaling-factor improved-fitness)]
+        (recur next-generation-population (inc generation) memory-crossover-rate memory-scaling-factor)))))
+
 
 (run 50 2 -5 5 (partial rastrigin-function 10.0 2.0) 1000)
